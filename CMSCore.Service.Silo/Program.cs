a440f4a39dcb19f732e06515992f0f5eca251451
Emil Södergren <Emil.Sodergren@bluestep.se>
2018-07-25 10:32:13 +0200

    Added project to solution

--- CMSCore.Service.Silo/Program.cs
+++ CMSCore.Service.Silo/Program.cs
@@ -1,12 +1,81 @@
-﻿using System;
-
-namespace CMSCore.Service.Silo
+﻿namespace CMSCore.Service.Silo
 {
-    class Program
+    using System;
+    using System.IO;
+    using System.Runtime.Loader;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Library.Data.Configuration;
+    using Library.Data.Context;
+    using Library.Grains;
+    using Library.Repository;
+    using Library.Repository.Implementations;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Orleans;
+    using Orleans.Configuration;
+    using Orleans.Hosting;
+
+    public class Program
     {
-        static void Main(string[] args)
+        private static ISiloHost silo;
+        private static readonly ManualResetEvent siloStopped = new ManualResetEvent(false);
+
+        static void Main(string [ ] args)
         {
-            Console.WriteLine("Hello World!");
+            silo = new SiloHostBuilder()
+                //.Configure<ClusterOptions>(options =>
+                //{
+                //    options.ClusterId = "orleans-docker";
+                //    options.ServiceId = "AspNetSampleApp";
+                //})
+                .UseLocalhostClustering()
+                //.ConfigureEndpoints(siloPort: 11111, gatewayPort: 30000)
+                .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(ReadContentGrain).Assembly).WithReferences().WithCodeGeneration())
+                .ConfigureLogging(builder => builder.SetMinimumLevel(LogLevel.Warning).AddConsole())
+                .ConfigureServices(services =>
+                {
+                    var conf = Configuration;
+                    services.AddSingleton<IConfiguration>(conf);
+
+ 
+                    services.AddSingleton<IDataConfiguration>(provider => new DataConfiguration(conf));
+                    services.AddSingleton<IContentDbContext>(provider =>
+                    {
+                        var p = provider.GetService<IDataConfiguration>();
+                        return new ContentDbContext(p);
+                    });
+                    services.AddSingleton<IReadContentRepository, ReadContentRepository>();
+                })
+                .Build();
+
+            Task.Run(StartSilo);
+
+            AssemblyLoadContext.Default.Unloading += context =>
+            {
+                Task.Run(StopSilo);
+                siloStopped.WaitOne();
+            };
+
+            siloStopped.WaitOne();
         }
+
+        private static async Task StartSilo()
+        {
+            await silo.StartAsync();
+            Console.WriteLine("************ Silo started ************");
+        }
+
+        private static async Task StopSilo()
+        {
+            await silo.StopAsync();
+            Console.WriteLine("************ Silo stopped ************");
+            siloStopped.Set();
+        }
+
+        public static IConfiguration Configuration => new ConfigurationBuilder()
+             .AddJsonFile("appsettings.json", false, true)
+            .Build();
     }
 }
