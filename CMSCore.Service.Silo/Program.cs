6efd634625184c83397ac996ba94d66cbce372da
Emil Södergren <Emil.Sodergren@bluestep.se>
2018-07-25 10:31:58 +0200

    Base functionality

---
+++ CMSCore.Service.Silo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CMSCore.Service.Silo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
